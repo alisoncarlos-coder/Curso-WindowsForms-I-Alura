ee272426c1ad1495941c152e351ec365b2e1bc64
alisoncarlos-coder <alison.carlos64@gmail.com>
2022-01-10 23:55:29 -0300

    Parte 04 - Curso: Windows Forms com C#: Formulários, componentes e eventos

---
+++ CursoWindowsForms/frm_Mascaras.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class frm_Mascaras : Form
+    {
+
+        //To Do -> Verificar como colocar atalhos nos botões. 
+        //Ex.: Pressiono uma tecla e aciono a ação de click do botão correspondente
+        public frm_Mascaras()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_VerConteudo_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = Msk_TextBox.Text;
+        }
+
+        private void Btn_Hora_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Mask = "00:00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_CEP_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Mask = "00000-000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Moeda_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Mask = "$ 000,000,000.00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Data_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Mask = "00/00/00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Senha_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = true;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Mask = "00000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Telefone_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Mask = "(00) 0000-0000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Focus();
+        }
+
+    }
+}
